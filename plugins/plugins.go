593f0dbfbe6446c96fd7e0232e8c5e5d87b2b6d9
Tom Moulard <tom.moulard@epita.fr>
2022-02-16 20:03:50 +0100

    chore: fixing module name

--- plugins/plugins.go
+++ plugins/plugins.go
@@ -1,11 +1,11 @@
 package plugins
 
 import (
-	_ "dwmstatus/plugins/battery"
-	_ "dwmstatus/plugins/cpu"
-	_ "dwmstatus/plugins/date"
-	_ "dwmstatus/plugins/memory"
-	_ "dwmstatus/plugins/time"
-	_ "dwmstatus/plugins/uptime"
-	_ "dwmstatus/plugins/wttr"
+	_ "github.com/idkso/dwmstatus/plugins/battery"
+	_ "github.com/idkso/dwmstatus/plugins/cpu"
+	_ "github.com/idkso/dwmstatus/plugins/date"
+	_ "github.com/idkso/dwmstatus/plugins/memory"
+	_ "github.com/idkso/dwmstatus/plugins/time"
+	_ "github.com/idkso/dwmstatus/plugins/uptime"
+	_ "github.com/idkso/dwmstatus/plugins/wttr"
 )
